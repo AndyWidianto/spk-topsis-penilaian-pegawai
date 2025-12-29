ba6415b59a22af6bc4f55dbcb0b2590d182eb701
AndyWidianto <gamestory5001@gmail.com>
2025-12-29 21:31:59 +0700

    clean prisma migrations lowercase tables

--- prisma/migrations/20251229143109_create_table_employee_assessments/migration.sql
+++ prisma/migrations/20251229143109_create_table_employee_assessments/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE `Users` (
+CREATE TABLE `users` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(191) NOT NULL,
     `username` VARCHAR(191) NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE `Users` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Employees` (
+CREATE TABLE `employees` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `nip` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE `Employees` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Criterias` (
+CREATE TABLE `criterias` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `code` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
@@ -40,7 +40,7 @@ CREATE TABLE `Criterias` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Priodes` (
+CREATE TABLE `priodes` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `month` INTEGER NOT NULL,
     `year` INTEGER NOT NULL,
@@ -52,7 +52,7 @@ CREATE TABLE `Priodes` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Assessments` (
+CREATE TABLE `assessments` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `employee_id` INTEGER NOT NULL,
     `priode_id` INTEGER NOT NULL,
@@ -61,12 +61,12 @@ CREATE TABLE `Assessments` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
-    UNIQUE INDEX `Assessments_employee_id_priode_id_key`(`employee_id`, `priode_id`),
+    UNIQUE INDEX `assessments_employee_id_priode_id_key`(`employee_id`, `priode_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `AssessmentDetails` (
+CREATE TABLE `assessment_details` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `assessment_id` INTEGER NOT NULL,
     `criteria_id` INTEGER NOT NULL,
@@ -78,13 +78,13 @@ CREATE TABLE `AssessmentDetails` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Assessments` ADD CONSTRAINT `Assessments_employee_id_fkey` FOREIGN KEY (`employee_id`) REFERENCES `Employees`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `assessments` ADD CONSTRAINT `assessments_employee_id_fkey` FOREIGN KEY (`employee_id`) REFERENCES `employees`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Assessments` ADD CONSTRAINT `Assessments_priode_id_fkey` FOREIGN KEY (`priode_id`) REFERENCES `Priodes`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `assessments` ADD CONSTRAINT `assessments_priode_id_fkey` FOREIGN KEY (`priode_id`) REFERENCES `priodes`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `AssessmentDetails` ADD CONSTRAINT `AssessmentDetails_assessment_id_fkey` FOREIGN KEY (`assessment_id`) REFERENCES `Assessments`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `assessment_details` ADD CONSTRAINT `assessment_details_assessment_id_fkey` FOREIGN KEY (`assessment_id`) REFERENCES `assessments`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `AssessmentDetails` ADD CONSTRAINT `AssessmentDetails_criteria_id_fkey` FOREIGN KEY (`criteria_id`) REFERENCES `Criterias`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `assessment_details` ADD CONSTRAINT `assessment_details_criteria_id_fkey` FOREIGN KEY (`criteria_id`) REFERENCES `criterias`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
